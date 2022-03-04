1c311eb15b00f8a54c8a52619a0c88449160e820
AlexBeje <alex.bejenaru93@gmail.com>
2022-03-04 11:01:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-j24pn9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-j24pn9)
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@mantine/core';
+
+function Home() {
+  return (
+    <div className="App">
+      <Button radius="lg">Hello world!</Button>
+    </div>
+  );
+}
+
+export default Home;
